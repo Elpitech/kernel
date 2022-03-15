07ac6dd9b2fb6cf9723d04980345f09553fedcf7
Vadim V. Vlasov <vadim.vlasov@elpitech.ru>
2022-03-15 14:13:19 +0300

    sound/hda: Merge with Baikal SDK-5.4

--- sound/hda/hdac_controller.c
+++ sound/hda/hdac_controller.c
@@ -11,6 +11,10 @@
 #include <sound/hda_register.h>
 #include "local.h"
 
+#ifdef CONFIG_OF
+#include <linux/of.h>
+#endif
+
 /* clear CORB read pointer properly */
 static void azx_clear_corbrp(struct hdac_bus *bus)
 {
@@ -31,11 +35,9 @@ static void azx_clear_corbrp(struct hdac_bus *bus)
 			break;
 		udelay(1);
 	}
-#if !IS_ENABLED(CONFIG_SND_HDA_BAIKAL_M)
 	if (timeout <= 0)
 		dev_err(bus->dev, "CORB reset timeout#2, CORBRP = %d\n",
 			snd_hdac_chip_readw(bus, CORBRP));
-#endif
 }
 
 /**
@@ -148,11 +150,11 @@ int snd_hdac_bus_send_cmd(struct hdac_bus *bus, unsigned int val)
 
 	spin_lock_irq(&bus->reg_lock);
 
-#if IS_ENABLED(CONFIG_SND_HDA_BAIKAL_M)
-	/* force first codec address, because wrong codec init */
-	val |= 0x10000000;
+#ifdef CONFIG_OF
+	if (of_property_read_bool(bus->dev->of_node, 
+			"increment-codec-address"))
+		val = val + 0x10000000;
 #endif
-
 	bus->last_cmd[azx_command_addr(val)] = val;
 
 	/* add command to corb */
